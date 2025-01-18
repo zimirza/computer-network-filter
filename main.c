00b41225995e62905741186a029e99aba2b53eb0
Zishan Mirza <zmirza@posteo.de>
2025-01-18 16:11:20 +0500

    get icmp packets

--- main.c
+++ main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int fd;
+    struct sockaddr addr;
+    socklen_t addr_len;
+    unsigned char *buffer;
+
+    buffer = (unsigned char *)malloc(65536);
+    addr_len = sizeof(addr);
+    fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (fd < 0) {
+        printf("unable to create socket\n");
+
+        return 1;
+    }
+
+    printf("server started...\n");
+
+    while (1) {
+        int bytes = recvfrom(fd, buffer, 65536, 0, (struct sockaddr *)&addr, &addr_len);
+        if (bytes < 0) {
+            printf("unable to receive data\n");
+
+            return 1;
+        }
+
+        struct iphdr *hdr = (struct iphdr *)buffer;
+        struct in_addr ip_addr;
+        ip_addr.s_addr = hdr->saddr;
+        printf("packet of size %d bytes from %s\n", bytes, inet_ntoa(ip_addr));
+    }
+
+    close(fd);
+    free(buffer);
 
     return 0;
 }
